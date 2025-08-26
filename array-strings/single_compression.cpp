fcdcccf1452ddf50c9f5cd02e407ef30624518e1
Anika Jain <anikajain1307@gmail.com>
2025-08-26 21:00:33 +0530

    single_compression

--- array-strings/single_compression.cpp
+++ array-strings/single_compression.cpp
@@ -1,1 +1,28 @@
-//aabcccccddd would become 
+//aabcccccddd would become a2b1c5d3
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+string compression(string& str){
+    map<char,int> mp;
+    for(int i=0;i<str.length();i++){
+        char ch = str[i];
+        mp[ch]++;
+    }
+    string res = "";
+    for(auto& ch:mp){
+        res+=ch.first;
+        res+=to_string(ch.second);
+    }
+    return res;
+}
+
+int main(){
+    string str = "aabcccdd";
+
+    cout<< compression(str);
+
+    return 0;
+}
