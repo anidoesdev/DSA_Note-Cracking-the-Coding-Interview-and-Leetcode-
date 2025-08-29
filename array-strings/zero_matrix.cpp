a1619cc8f3710da7e7e51877a35ef5de1d09a173
Anika Jain <anikajain1307@gmail.com>
2025-08-30 00:34:19 +0530

    ignore

---
+++ array-strings/zero_matrix.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+void zero_matrix(vector<vector<int>>& matrix){
+
+}
+
+int main() {
+    cout<< "Hello";
+    return 0;
+}
