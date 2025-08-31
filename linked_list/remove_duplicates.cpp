cb17b367d0867ed7301411c21d66f0617710e494
Anika Jain <anikajain1307@gmail.com>
2025-09-01 00:10:27 +0530

    remove_dups_linked_list

---
+++ linked_list/remove_duplicates.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+    // ListNode() : val(0), next(nullptr) {}
+    // ListNode(int x) : val(x), next(nullptr) {}
+    // ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+void remove_dups(ListNode* head){
+    set<int> st;
+    ListNode* prev = NULL;
+    while(head != nullptr){
+        if(st.find(head->val) != st.end()){
+            prev->next = head->next;
+        }else{
+            st.insert(head->val);
+            prev = head;
+        }
+        
+        
+        head = head->next;
+    }
+}
+void printList(ListNode* node){
+    while(node!=nullptr){
+        cout<< node->val << "->";
+        node = node->next;
+    }
+    cout<< "NULL"<< endl;
+}
+
+
+int main(){
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(2);
+    head->next->next->next = new ListNode(3);
+    head->next->next->next->next = new ListNode(4);
+
+    printList(head);
+
+    remove_dups(head);
+
+    printList(head);
+
+    return 0;
+}
+
+
+//no buffer required 
+
