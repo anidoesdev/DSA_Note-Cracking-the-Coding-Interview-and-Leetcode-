f4c557061b5d20ed5f9295b991f932f22b90b8e6
Anika Jain <121229462+anidoesdev@users.noreply.github.com>
2026-03-15 17:25:00 +0530

    Automated fix

--- array-strings/Palindrome_Permutation.cpp
+++ array-strings/Palindrome_Permutation.cpp
@@ -7,8 +7,10 @@ using namespace std;
 bool permutation_of_palindrome (string& str){
     map<char,int> mp;
     int n = str.length();
-    for(int i=0;i<str.length();i++){
+    for(int i=0;i<n;i++){
         char ch = str[i];
+        if(ch >= 'A' && ch <= 'Z') ch = ch - 'A' + 'a';
+        if(ch == ' ') continue;
         mp[ch]++;
     }
     int count = 0;
@@ -27,13 +29,10 @@ bool permutation_of_palindrome (string& str){
         }
     }
     return true;
-
 }
 
 int main(){
-    
     string str = "tactcoa";
     cout<< permutation_of_palindrome(str);
     return 0;
 }
-
