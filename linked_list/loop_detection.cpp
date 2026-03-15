0fe0cb37b2763d40fe3e9f44c175bcb653a27b1b
Anika Jain <121229462+anidoesdev@users.noreply.github.com>
2026-03-15 11:59:36 +0530

    Automated fix

--- linked_list/loop_detection.cpp
+++ linked_list/loop_detection.cpp
@@ -1,54 +1,41 @@
-#include<iostream>
+```python
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
+def loop_detection(head):
+    slow = head
+    fast = head
 
-using namespace std;
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
-ListNode* loop_detection(ListNode* head){
-    ListNode* slow = head;
-    ListNode* fast= head;
+    while fast is not None and fast.next is not None:
+        slow = slow.next
+        fast = fast.next.next
+        if slow == fast:
+            break
+    if fast is None or fast.next is None:
+        return None
+    slow = head
+    while slow != fast:
+        slow = slow.next
+        fast = fast.next
+    return fast
 
-    while(fast!=NULL && fast->next!=NULL){
-        slow = slow->next;
-        fast = fast->next->next;
-        if(slow == fast){
-            break;
-        }
-    }
-    if(fast==NULL || fast->next == NULL){
-        return NULL;
-    }
-    slow = head;
-    while (slow!=fast){
-       slow = slow->next;
-       fast = fast->next;
-    }
-    return fast;
-}
-void printList(ListNode* node){
-    while(node!=nullptr){
-        cout<< node->val << "->";
-        node = node->next;
-    }
-    cout<< "NULL"<< endl;
-}
+def print_list(node):
+    while node is not None:
+        print(node.val, end='->')
+        node = node.next
+    print('NULL')
 
-int main(){
-    ListNode* head = new ListNode(1);
-    head->next = new ListNode(3);
-    head->next->next = new ListNode(4);
-    head->next->next->next = new ListNode(5);
-  
-    head->next->next->next->next = head->next;
-    
-    ListNode* res = loop_detection(head);
-    cout<< res->val;
+def main():
+    head = ListNode(1)
+    head.next = ListNode(3)
+    head.next.next = ListNode(4)
+    head.next.next.next = ListNode(5)
+    head.next.next.next.next = head.next
+    res = loop_detection(head)
+    print(res.val)
 
-    
-    return 0;
-}
+if __name__ == "__main__":
+    main()
+```
