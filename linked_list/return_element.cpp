be3f261c411ae81dfbc153934b23698e39ea583d
Anika Jain <121229462+anidoesdev@users.noreply.github.com>
2026-03-15 16:20:35 +0530

    Automated fix

--- linked_list/return_element.cpp
+++ linked_list/return_element.cpp
@@ -1,4 +1,3 @@
-//return kth to the last element
 #include <iostream>
 
 using namespace std;
@@ -10,32 +9,29 @@ struct ListNode {
     ListNode(int x) : val(x), next(nullptr) {}
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
-//bruteforce solution
-int return_element(ListNode* head,int k){
+
+int return_element(ListNode* head, int k) {
     int count = 0;
     ListNode* temp = head;
-    while(temp != NULL){
+    while (temp != NULL) {
         temp = temp->next;
         count++;
     }
     temp = head;
-    for(int i=1;i<count-k+1;i++){
+    for (int i = 1; i < count - k + 1; i++) {
         temp = temp->next;
     }
     return temp->val;
-    
 }
 
-int main(){
+int main() {
     ListNode* head = new ListNode(1);
     head->next = new ListNode(2);
     head->next->next = new ListNode(3);
     head->next->next->next = new ListNode(4);
     head->next->next->next->next = new ListNode(5);
-    
 
-    cout<< return_element(head,2);
-
+    cout << return_element(head, 2);
 
     return 0;
 }
