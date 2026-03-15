6dc894206f95d531092fbbf1a76a47a4678f7df1
Anika Jain <121229462+anidoesdev@users.noreply.github.com>
2026-03-15 17:25:10 +0530

    Automated fix

--- array-strings/zero_matrix.cpp
+++ array-strings/zero_matrix.cpp
@@ -4,6 +4,7 @@
 #include <map>
 
 using namespace std;
+
 void zero_matrix(vector<vector<int>>& matrix){
     map<int,int> row;
     map<int,int> column;
@@ -24,16 +25,14 @@ void zero_matrix(vector<vector<int>>& matrix){
             }
         }
     }
-
 }
 
 int main() {
-    vector<vector<int>> matrix(3,vector<int>(3));
-    matrix = {{0,1,2},{2,3,4},{6,0,7}};
+    vector<vector<int>> matrix = {{0,1,2},{2,3,4},{6,0,7}};
 
     for(int i=0;i<matrix.size();i++){
         for(int j=0;j<matrix[0].size();j++){
-            cout<< matrix[i][j];
+            cout<< matrix[i][j] << " ";
         }
         cout<<endl;
     }
@@ -41,11 +40,10 @@ int main() {
     zero_matrix(matrix);
     for(int i=0;i<matrix.size();i++){
         for(int j=0;j<matrix[0].size();j++){
-            cout<< matrix[i][j];
+            cout<< matrix[i][j] << " ";
         }
         cout<<endl;
     }
-
 
     return 0;
 }
