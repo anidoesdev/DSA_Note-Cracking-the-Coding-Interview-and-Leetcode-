d5747679e073d19640d236201749378a3329b51a
Anika Jain <121229462+anidoesdev@users.noreply.github.com>
2026-03-15 17:27:32 +0530

    Automated fix

--- array-strings/zero_matrix.cpp
+++ array-strings/zero_matrix.cpp
@@ -4,32 +4,31 @@
 #include <map>
 
 using namespace std;
+
 void zero_matrix(vector<vector<int>>& matrix){
-    map<int,int> row;
-    map<int,int> column;
+    vector<bool> row(matrix.size(), false);
+    vector<bool> column(matrix[0].size(), false);
     int m = matrix.size();
     int n = matrix[0].size();
     for(int i=0;i<m;i++){
         for(int j=0;j<n;j++){
             if(matrix[i][j]==0){
-                row[i]++;
-                column[j]++;
+                row[i] = true;
+                column[j] = true;
             }
         }
     }
     for(int i=0;i<m;i++){
         for(int j=0;j<n;j++){
-            if(row.find(i)!=row.end() || column.find(j)!=column.end()){
+            if(row[i] || column[j]){
                 matrix[i][j] = 0;
             }
         }
     }
-
 }
 
 int main() {
-    vector<vector<int>> matrix(3,vector<int>(3));
-    matrix = {{0,1,2},{2,3,4},{6,0,7}};
+    vector<vector<int>> matrix = {{0,1,2},{2,3,4},{6,0,7}};
 
     for(int i=0;i<matrix.size();i++){
         for(int j=0;j<matrix[0].size();j++){
@@ -45,7 +44,6 @@ int main() {
         }
         cout<<endl;
     }
-
 
     return 0;
 }
