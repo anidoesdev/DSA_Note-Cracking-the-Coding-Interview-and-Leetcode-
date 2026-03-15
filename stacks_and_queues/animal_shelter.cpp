2f11b06edab35f974b876a22a9311fe33fb7e8cc
Anika Jain <121229462+anidoesdev@users.noreply.github.com>
2026-03-15 11:59:48 +0530

    Automated fix

--- stacks_and_queues/animal_shelter.cpp
+++ stacks_and_queues/animal_shelter.cpp
@@ -1,98 +1,80 @@
-//Have to practice these type of questions
+```python
+class Animal:
+    def __init__(self, name="", order=0):
+        self.name = name
+        self.order = order
+
+    def set_order(self, order):
+        self.order = order
+
+    def get_order(self):
+        return self.order
+
+    def is_order_than(self, other):
+        return self.order < other.order
 
 
-#include<iostream>
-using namespace std;
+class Dog(Animal):
+    def __init__(self, name):
+        super().__init__(name)
 
-class Animal {
-protected:
-    string name;
-    int order;
-public:
-    Animal() : name(""), order(0) {}
-    Animal(string n) : name(n), order(0) {}
-    void setOrder(int o) { order = o; }
-    int getOrder() { return order; }
-    bool isOrderThan(Animal* a) {
-        return order < a->getOrder();
-    }
-    virtual ~Animal() {}
-};
 
-class Dog : public Animal {
-public:
-    Dog(string n) : Animal(n) {}
-};
+class Cat(Animal):
+    def __init__(self, name):
+        super().__init__(name)
 
-class Cat : public Animal {
-public:
-    Cat(string n) : Animal(n) {}
-};
 
-struct LinkedList {
-    Animal* animal;
-    LinkedList* next;
-    LinkedList(Animal* a) : animal(a), next(nullptr) {}
-};
+class Node:
+    def __init__(self, animal):
+        self.animal = animal
+        self.next = None
 
-class animalQueue {
-    LinkedList* dogs = nullptr;
-    LinkedList* cats = nullptr;
-    int order = 0;
-public:
-    void enqueue(Animal* a) {
-        a->setOrder(order++);
-        if (dynamic_cast<Dog*>(a)) {
-            // Add to dogs list
-            LinkedList* node = new LinkedList(a);
-            node->next = dogs;
-            dogs = node;
-        } else if (dynamic_cast<Cat*>(a)) {
-            // Add to cats list
-            LinkedList* node = new LinkedList(a);
-            node->next = cats;
-            cats = node;
-        }
-    }
-    Animal* dequeue() {
-        // If both lists are empty
-        if (!dogs && !cats) return nullptr;
-        // If only dogs are present
-        if (dogs && !cats) {
-            LinkedList* node = dogs;
-            dogs = dogs->next;
-            Animal* a = node->animal;
-            delete node;
-            return a;
-        }
-        // If only cats are present
-        if (cats && !dogs) {
-            LinkedList* node = cats;
-            cats = cats->next;
-            Animal* a = node->animal;
-            delete node;
-            return a;
-        }
-        // Both lists have animals, compare order
-        if (dogs->animal->getOrder() < cats->animal->getOrder()) {
-            LinkedList* node = dogs;
-            dogs = dogs->next;
-            Animal* a = node->animal;
-            delete node;
-            return a;
-        } else {
-            LinkedList* node = cats;
-            cats = cats->next;
-            Animal* a = node->animal;
-            delete node;
-            return a;
-        }
-    }
-};
 
-int main() {
-    animalQueue shelter;
-    shelter.enqueue(new Dog("Rex"));
-    shelter.enqueue(new Cat("Whiskers"));
-    return 0;
-}
+class AnimalQueue:
+    def __init__(self):
+        self.dogs = None
+        self.cats = None
+        self.order = 0
+
+    def enqueue(self, animal):
+        animal.set_order(self.order)
+        self.order += 1
+        if isinstance(animal, Dog):
+            node = Node(animal)
+            node.next = self.dogs
+            self.dogs = node
+        elif isinstance(animal, Cat):
+            node = Node(animal)
+            node.next = self.cats
+            self.cats = node
+
+    def dequeue(self):
+        if not self.dogs and not self.cats:
+            return None
+        if self.dogs and not self.cats:
+            node = self.dogs
+            self.dogs = self.dogs.next
+            return node.animal
+        if self.cats and not self.dogs:
+            node = self.cats
+            self.cats = self.cats.next
+            return node.animal
+        if self.dogs.animal.get_order() < self.cats.animal.get_order():
+            node = self.dogs
+            self.dogs = self.dogs.next
+            return node.animal
+        else:
+            node = self.cats
+            self.cats = self.cats.next
+            return node.animal
+
+
+def main():
+    shelter = AnimalQueue()
+    shelter.enqueue(Dog("Rex"))
+    shelter.enqueue(Cat("Whiskers"))
+
+
+if __name__ == "__main__":
+    main()
+```
