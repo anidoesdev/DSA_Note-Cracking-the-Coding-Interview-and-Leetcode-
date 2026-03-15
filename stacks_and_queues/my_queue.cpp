24aaa610fc7f6fdad7ba431d874990e463825e47
Anika Jain <121229462+anidoesdev@users.noreply.github.com>
2026-03-15 11:59:50 +0530

    Automated fix

--- stacks_and_queues/my_queue.cpp
+++ stacks_and_queues/my_queue.cpp
@@ -1,48 +1,32 @@
-#include<iostream>
-#include<stack>
+```python
+class MyQueue:
+    def __init__(self):
+        self.st1 = []
+        self.st2 = []
 
-using namespace std;
-class MyQueue{
-    stack<int>* st1;
-    stack<int>* st2;
-    void shiftStack(){
-            if(st2->empty()){
-                while(!st1->empty()){
-                    st2->push(st1->top());
-                    st1->pop();
-                }
-            }
-    }
-    public:
-        MyQueue(){
-            st1 = new stack<int>;
-            st2 = new stack<int>;
-        }
-        void push(int value){
-            st1->push(value);
-        }
-        int peek(){
-            shiftStack();
-            return st2->top();
-        }
-        int remove(){
-            shiftStack();
-            int value = st2->top();
-            st2->pop();
-            return value;
-        }
-};
+    def shift_stack(self):
+        if not self.st2:
+            while self.st1:
+                self.st2.append(self.st1.pop())
+
+    def push(self, value):
+        self.st1.append(value)
+
+    def peek(self):
+        self.shift_stack()
+        return self.st2[-1]
+
+    def remove(self):
+        self.shift_stack()
+        return self.st2.pop()
 
 
-int main(){
-    MyQueue queue;
-    queue.push(1);
-    queue.push(2);
-    queue.push(3);
-    queue.push(4);
-    cout<< queue.peek()<<endl;
-    queue.remove();
-    cout<< queue.peek();
-
-    return 0;
-}
+queue = MyQueue()
+queue.push(1)
+queue.push(2)
+queue.push(3)
+queue.push(4)
+print(queue.peek())
+queue.remove()
+print(queue.peek())
+```
