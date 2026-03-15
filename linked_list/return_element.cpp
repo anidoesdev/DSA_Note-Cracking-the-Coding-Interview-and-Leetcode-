4fee3043e7723dc7e07eedb2418284b43a1f4ff6
Anika Jain <121229462+anidoesdev@users.noreply.github.com>
2026-03-15 11:59:44 +0530

    Automated fix

--- linked_list/return_element.cpp
+++ linked_list/return_element.cpp
@@ -1,41 +1,29 @@
-//return kth to the last element
-#include <iostream>
+```python
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
-using namespace std;
+def return_element(head, k):
+    count = 0
+    temp = head
+    while temp is not None:
+        temp = temp.next
+        count += 1
+    temp = head
+    for i in range(1, count - k + 1):
+        temp = temp.next
+    return temp.val
 
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
-//bruteforce solution
-int return_element(ListNode* head,int k){
-    int count = 0;
-    ListNode* temp = head;
-    while(temp != NULL){
-        temp = temp->next;
-        count++;
-    }
-    temp = head;
-    for(int i=1;i<count-k+1;i++){
-        temp = temp->next;
-    }
-    return temp->val;
-    
-}
+def main():
+    head = ListNode(1)
+    head.next = ListNode(2)
+    head.next.next = ListNode(3)
+    head.next.next.next = ListNode(4)
+    head.next.next.next.next = ListNode(5)
 
-int main(){
-    ListNode* head = new ListNode(1);
-    head->next = new ListNode(2);
-    head->next->next = new ListNode(3);
-    head->next->next->next = new ListNode(4);
-    head->next->next->next->next = new ListNode(5);
-    
+    print(return_element(head, 2))
 
-    cout<< return_element(head,2);
-
-
-    return 0;
-}
+if __name__ == "__main__":
+    main()
+```
