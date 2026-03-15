c789305802d7070c3cdeb84f4c48481ba59acac3
Anika Jain <121229462+anidoesdev@users.noreply.github.com>
2026-03-15 11:59:00 +0530

    Automated fix

--- array-strings/string_compression.cpp
+++ array-strings/string_compression.cpp
@@ -1,28 +1,17 @@
-//aabcccccddd would become a2b1c5d3
-#include <iostream>
-#include <map>
-#include <string>
+```python
+def compression(s):
+    mp = {}
+    for ch in s:
+        if ch in mp:
+            mp[ch] += 1
+        else:
+            mp[ch] = 1
+    res = ""
+    for ch in sorted(mp.keys()):
+        res += ch
+        res += str(mp[ch])
+    return res
 
-using namespace std;
-
-string compression(string& str){
-    map<char,int> mp;
-    for(int i=0;i<str.length();i++){
-        char ch = str[i];
-        mp[ch]++;
-    }
-    string res = "";
-    for(auto& ch:mp){
-        res+=ch.first;
-        res+=to_string(ch.second);
-    }
-    return res;
-}
-
-int main(){
-    string str = "aabcccdd";
-
-    cout<< compression(str);
-
-    return 0;
-}
+str = "aabcccdd"
+print(compression(str))
+```
