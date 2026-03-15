5a675796ee6b094132ce75a47cd517677d4dca0d
Anika Jain <121229462+anidoesdev@users.noreply.github.com>
2026-03-15 11:59:07 +0530

    Automated fix

--- array-strings/string_rotation.cpp
+++ array-strings/string_rotation.cpp
@@ -1,26 +1,11 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
+```python
+def is_rotation(s1, s2):
+    if len(s1) == len(s2) and len(s1) > 0:
+        return s2 in s1 + s1
+    return False
 
-using namespace std;
+s1 = "waterbottle"
+s2 = "erbottlewat"
 
-
-bool is_rotation(string& s1, string& s2){
-    int len = s1.length();
-    if(len == s2.length() && len>0){
-        string str = s1 + s1;
-        return str.find(s2);
-    }
-    return false;
-
-}
-
-int main(){
-
-    string s1 = "waterbottle";
-    string s2 = "erbottlewat";
-
-    cout<< is_rotation(s1,s2);
-
-    return 0;
-}
+print(is_rotation(s1, s2))
+```
